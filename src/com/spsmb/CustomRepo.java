df984357a777850769db2350260174f96926c524
jakub.simon <simon.jakub.684@gmail.com>
2021-12-01 14:20:45 +0100

    hitler is alive

--- src/com/spsmb/CustomRepo.java
+++ src/com/spsmb/CustomRepo.java
@@ -8,10 +8,4 @@ import java.util.logging.Logger;
 public interface CustomRepo {
 
     Logger logger = Logger.getLogger(Main.class.getName());
-
-//    static void action() {
-//        log.info("TEST");
-//    }
-//    logger.setLevel(Level.FINE);
-
 }
