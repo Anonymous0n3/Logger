f73df7872653d25a2c414f4d44c8762f070f3bc4
tomas.kargl <tomas.kargl@student.spsmb.cz>
2021-12-01 14:33:39 +0100

    more nevim už

--- src/com/spsmb/FormMsg.java
+++ src/com/spsmb/FormMsg.java
@@ -3,9 +3,6 @@ package com.spsmb;
 public class FormMsg extends Main {
     public static void msg(String[] args) {
 
-        String name = new String();
-        String form1 = String.format("My name is %s",name);
-        System.out.println(form1);
 
     }
 
