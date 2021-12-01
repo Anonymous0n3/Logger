14202f939a6a7d593141846df908a02ab28e561c
Vojtěch Žmolík <vojtech.zmolik@student.spsmb.cz>
2021-12-01 14:25:45 +0100

    DO METHODS

--- src/com/spsmb/ConsoleLogger.java
+++ src/com/spsmb/ConsoleLogger.java
@@ -3,17 +3,17 @@ package com.spsmb;
 public class ConsoleLogger implements Logger {
     @Override
     public void warn(String message) {
-
+        //Jakub
     }
 
     @Override
     public void info(String message) {
-
+        //Tomáš
     }
 
     @Override
     public void error(String message) {
-
+        //Vojta
     }
 
     @Override
