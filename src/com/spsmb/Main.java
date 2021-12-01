b1b3ff7036f115d6290f4416b7f772071f687e72
tomas.kargl <tomas.kargl@student.spsmb.cz>
2021-12-01 14:32:12 +0100

    more nevim už

--- src/com/spsmb/Main.java
+++ src/com/spsmb/Main.java
@@ -1,14 +1,4 @@
 package com.spsmb;
 
 public class Main {
-
-    public static Logger logger = new ConsoleLogger();
-
-    public static void main(String[] args) {
-        logger.setLevel(LoggerLevel.ERROR);
-        logger.warn("toto je warn");
-        logger.info("toto je info");
-        logger.error("toto je error");
-    }
-
 }
